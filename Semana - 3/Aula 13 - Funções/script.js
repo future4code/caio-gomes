cea92c4de80e788b0aca6074e57a19e5fca17a0e
caiobrian <caiogomeslog@gmail.com>
2019-09-23 14:12:42 -0300

    primeiro requisito do desafio feito.

--- Semana - 3/Aula 13 - Funções/script.js
+++ Semana - 3/Aula 13 - Funções/script.js
@@ -1,9 +1,14 @@
-function gerarTarefa (){
+function semInput() {
+    if (document.querySelector("input").value === ""){
+        alert("Por favor, insira uma tarefa!");
+    }else {
+        gerarTarefa();
+    }
+}
+
+function gerarTarefa(){
     const valorDoInput = document.querySelector("input").value;
-    console.log(valorDoInput);
     const diaEscolhido = document.querySelector("select").value;
-    console.log(diaEscolhido);
-    
     const segundaFeira = document.getElementById("segunda");
     const tercaFeira = document.getElementById("terca");
     const quartaFeira = document.getElementById("quarta");
@@ -11,23 +16,22 @@ function gerarTarefa (){
     const sextaFeira = document.getElementById("sexta");
     const sabadao = document.getElementById("sabado");
     const domingueira = document.getElementById("domingo")
-    document.querySelector("input").value = "";
-
+    
         if (diaEscolhido === "segunda"){
             segundaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
         }else if (diaEscolhido === "terca"){
             tercaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
         }else if (diaEscolhido === "quarta"){
-            quartaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
-        }else if (diaEscolhido === "quinta"){
-            quintaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
-        }else if (diaEscolhido === "sexta"){
-            sextaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
-        }else if (diaEscolhido === "sabado"){
-            sabadao.innerHTML += "<li>" + valorDoInput + "</li>";
-        }else {
-            domingueira.innerHTML += "<li>" + valorDoInput + "</li>";
-        }
-
+                quartaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
+            }else if (diaEscolhido === "quinta"){
+                quintaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
+            }else if (diaEscolhido === "sexta"){
+                sextaFeira.innerHTML += "<li>" + valorDoInput + "</li>";
+            }else if (diaEscolhido === "sabado"){
+                sabadao.innerHTML += "<li>" + valorDoInput + "</li>";
+            }else {
+                domingueira.innerHTML += "<li>" + valorDoInput + "</li>";
+            }
         
+        document.querySelector("input").value = "";
 }
