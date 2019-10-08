7f3b05fff3709944fcaf01c88c5e9cffdd2bf9eb
caiobrian <caiogomeslog@gmail.com>
2019-10-08 14:40:59 -0300

    componentes feitos

---
+++ Semana - 5/Aula 22 Renderização Condicional/form-future4/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import FormEtapa1 from './components/FormEtapa1'
+import FormEtapa2 from './components/FormEtapa2'
+import FormEtapa3 from './components/FormEtapa3'
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ensinoMedioIncompleto: true,
+      ensinoMedioCompleto: true,
+      ensinoSuperiorIncompleto: true,
+      ensinoSuperiorCompleto: true      
+    }
+  }
+ 
+  
+ 
+render() {  
+    
+
+  return (
+     <div className="App">
+        <FormEtapa1
+        medioIncompleto={this.state.ensinoMedioIncompleto}
+        medioCompleto={this.state.ensinoMedioCompleto}
+        superiorIncompleto={this.state.ensinoSuperiorIncompleto}
+        superiorCompleto={this.state.ensinoSuperiorCompleto}
+        onClickButtonContinue={this.clickButtonContinue}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Semana - 5/Aula 22 Renderização Condicional/form-future4/src/components/FormEtapa2.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export function FormEtapa2() {
+    return (
+        <div>
+            <h1> Etapa 2 - INFORMAÇÕES DO ENSINO SUPERIOR </h1>
+            <div>
+                <p>5. Qual curso?</p>
+                <input type="text"/>
+                <p>6. Qual a unidade de ensino?</p>
+                <input type="text"/>
+            </div>
+            <div>
+                <button>Continuar</button>
+            </div>
+        </div>
+    )
+}
+
+export default FormEtapa2;
