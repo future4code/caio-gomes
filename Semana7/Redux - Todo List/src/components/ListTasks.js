4fa6742249de18d1daaa3fe1b94b548809ef0511
caiobrian <caiogomeslog@gmail.com>
2019-10-28 16:07:13 -0300

    alterando estilização

--- Semana7/Redux - Todo List/src/components/ListTasks.js
+++ Semana7/Redux - Todo List/src/components/ListTasks.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { useDispatch, useReduxState } from "../redux/react-redux-f4";
+import styled from 'styled-components';
+
+
+const ContainerTask = styled.div`
+	display: flex;
+	justify-content: space-around;
+	border: 1px solid black;
+`
+const TaskP = styled.div`
+	margin: 10px;
+`
+
+
 
 const ListTasks = (props) => {
 	const state = useReduxState();
 	
 	
 	const listTask = state.listTasks.map((task)=>{
-		return <div><p>{task}</p></div>
+		return <ContainerTask>
+			<button>Checked</button>
+			<TaskP>{task}</TaskP>
+			<button>X</button>
+			
+			</ContainerTask>
 	})
 	
 	return(
--- Semana7/Redux - Todo List/src/components/MenuTask.js
+++ Semana7/Redux - Todo List/src/components/MenuTask.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { useReduxState } from '../redux/react-redux-f4';
+import styled from 'styled-components';
+
+const ContainerMenu = styled.div`
+	display: flex;
+	border: 1px solid red;
+`
+const ButtonMenu = styled.button`
+
+
+`
 
 const MenuTask = () => {
 	const state = useReduxState();
 
 	return (
-		<div>
-			<button>Todas</button>
-			<button>Pendentes</button>
-			<button>Completas</button>
-		</div>
+		<ContainerMenu>
+			<ButtonMenu>Marcar todas como completas</ButtonMenu>
+			<ButtonMenu>Todas</ButtonMenu>
+			<ButtonMenu>Pendentes</ButtonMenu>
+			<ButtonMenu>Completas</ButtonMenu>
+			<ButtonMenu>Remover completas</ButtonMenu>
+		</ContainerMenu>
 	)
 }
 
