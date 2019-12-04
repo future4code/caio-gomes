1ee955eb46c27cc35b86465e6c7bdeff29781f4a
caiobrian <caiogomeslog@gmail.com>
2019-12-04 16:49:50 -0300

    finalizando primeiro exercicio

--- Semana - 12/Aula 54/Exercicios de Promise/build/ex1.js
+++ Semana - 12/Aula 54/Exercicios de Promise/build/ex1.js
@@ -1,22 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const fileName = './textos';
-const readDirectory = new Promise((resolve, reject) => {
-    fs_1.readdir(fileName, (err, files) => {
-        if (err) {
-            reject(err);
-        }
-        else {
-            resolve(files);
-        }
-    });
-});
-readDirectory
-    .then(content => {
-    console.log(content);
-})
-    .catch(e => {
-    console.error(e);
+const path = require('path');
+const directoryPath = path.join(__dirname, '../textos');
+fs_1.readdir(directoryPath, function (err, files) {
+    if (err) {
+        console.log('Erro');
+    }
+    else {
+        files.forEach(function (file) {
+            fs_1.readFile(directoryPath + `/${file}`, (err, data) => {
+                if (err) {
+                    console.log('ERRO AO ACESSAR TEXTOS');
+                }
+                else {
+                    console.log(data.toString());
+                }
+            });
+        });
+    }
 });
 //# sourceMappingURL=ex1.js.map
--- Semana - 12/Aula 54/Exercicios de Promise/src/ex1.ts
+++ Semana - 12/Aula 54/Exercicios de Promise/src/ex1.ts
@@ -1,21 +1,21 @@
 import { readdir, readFile } from 'fs';
+const path = require('path');
 
-const fileName: string = './textos';
+const directoryPath = path.join(__dirname, '../textos');
 
-const readDirectory = new Promise((resolve, reject) => {
-  readdir(fileName, (err, files) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(files);
-    }
-  });
+readdir(directoryPath, function(err: Error, files: any) {
+  if (err) {
+    console.log('Erro');
+  } else {
+    files.forEach(function(file: string) {
+      readFile(directoryPath + `/${file}`, (err: Error, data: Buffer)=>{
+        if(err){
+          console.log('ERRO AO ACESSAR TEXTOS')
+        } else {
+          console.log(data.toString())
+        }
+      });
+    });
+  }
 });
 
-readDirectory
-  .then(content => {
-    console.log(content);
-  })
-  .catch(e => {
-    console.error(e);
-  });
