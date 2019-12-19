b85e6372e88980adb82a634077d3e8d4b0c3e60c
caiobrian <caiogomeslog@gmail.com>
2019-12-19 11:57:13 -0300

    first commit

--- Semana - 16/Aula Express/src/index.ts
+++ Semana - 16/Aula Express/src/index.ts
@@ -59,6 +59,50 @@ app.post('/mirror/:cor', (req: Request, res: Response) => {
   res.send(responseBody);
 });
 
+// CT - Ex1
+app.get('/getAllBrazilianClients', async (req: Request, res: Response) => {
+  const query = connection.select('primeiro_nome')
+                          .from('correntistas')
+                          .where('pais', 'Brazil');
+  const result = await query;
+  res.send(result);
+});
+// CT - Ex2
+app.get('/searchClientByName', (req: Request, res: Response) => {
+  const nameToSearch = req.query.name;
+  if(!nameToSearch){
+    res.status(400).end();
+    return;
+  }
+  const query = connection.raw(`SELECT primeiro_nome FROM correntistas WHERE primeiro_nome LIKE "%${nameToSearch}%"`)
+  query.then(result => {
+    res.send(result)
+  })
+});
+// CT - Ex3
+app.get('/getBirthdaysAfterDate/:date', (req: Request, res: Response) => {
+  const dateToBeCompared = req.params.date;
+  const query = connection.raw(`SELECT primeiro_nome, aniversario FROM correntistas WHERE aniversario < "${dateToBeCompared}"`)
+  query.then(result => {
+    res.send(result);
+  })
+});
+// CT - Ex4
+app.post('/createClient', (req: Request, res: Response) => {
+  const newCliente = {
+    ...req.body,
+    divida_perdoada: 0,
+    conta_corrente: 0,
+  };
+  const query = connection('correntistas').insert(newCliente);
+  query.then(result => {
+    res.send(result);
+  }).catch(e => {
+    res.send(e);
+  })
+});
+
+
 // Trecho do código responsável por inicializar todas as APIs
 const server = app.listen(process.env.PORT || 3000, () => {
   if(server){
