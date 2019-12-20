0e4805c2897f713e974a6d9f195b88aeede59d36
caiobrian <caiogomeslog@gmail.com>
2019-12-20 14:54:11 -0300

    iniciando tasks

--- Semana - 16/Aula Express/src/index.ts
+++ Semana - 16/Aula Express/src/index.ts
@@ -7,13 +7,11 @@ app.use(express.json()); // Linha mágica (middleware)
 
 const connection = knex({
   client: 'mysql',
-  acquireConnectionTimeout: 10000000,
   connection: {
     host: 'ec2-18-229-236-15.sa-east-1.compute.amazonaws.com',
     user: 'caio',
     password: process.env.SENHA_BANCO,
     database: 'caio',
-    debug: true,
   },
 });
 
@@ -29,15 +27,38 @@ app.post('/createUser', (req: Request, res: Response) => {
     });
 });
 
-app.get('/getAllBrazilianClients', (req: Request, res: Response) => {
-  const query = connection.select('primeiro_nome')
-                          .from('correntistas')
-                          .where('pais', 'Brazil');
-    query.then((result)=>{
+app.put('/editNickname/:id', (req: Request, res: Response) => {
+  const newnickname = req.body.nickname
+  const idcompare = req.params.id
+  const query = connection('users').where({id: idcompare}).update({nickname: newnickname})
+  query.then(result => {
     res.send(result);
-  })
+  }).catch(err => res.status(500).send(err));
 });
 
+app.delete('/deleteUser/:id', (req: Request, res: Response) => {
+  const id = req.params.id
+  const query = connection('users').where({id: id}).del()
+  query.then(result => {
+    res.send(result);
+  }).catch(err => res.status(500).send(err));
+})
+
+app.get('/getAllUsers', (req: Request, res: Response) => {
+  const query = connection.select('*').from('users');
+  query.then(result => {
+    res.send(result);
+  });
+});
+
+app.get('/getUser/', (req: Request, res: Response) => {
+  let id = req.params.id;
+  let name = req.query.name;
+  const query = connection.select('*').from('users').where({id: id}).orWhere({name: name});
+  query.then(result => {
+    res.send(result);
+  }).catch(err => res.status(500).send(err));
+})
 
 // Trecho do código responsável por inicializar todas as APIs
 const server = app.listen(process.env.PORT || 3000, () => {
