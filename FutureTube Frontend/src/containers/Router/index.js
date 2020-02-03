3e786133b644cffa458d1974cd0dd42f93cb91eb
caiobrian <caiogomeslog@gmail.com>
2020-02-03 17:09:08 -0300

    subindo boilerplate atualizado de front

--- FutureTube Frontend/src/containers/Router/index.js
+++ FutureTube Frontend/src/containers/Router/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
+import Login from "../../Page/Login";
 
-const routes = {};
+export const routes = {
+  login: '/'
+};
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-        <Route path={routes.root} component={() => <div />} />
+        <Route path={routes.login} exact component={Login} />
       </Switch>
     </ConnectedRouter>
   );
